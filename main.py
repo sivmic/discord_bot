13534974378ffa843f7a7d92b7671052d3a207e4
sivmic <sivak.dacice@outlook.com>
2017-04-25 21:28:04 +0200

    Deployment script improved for services

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python3
+
 import discord
 import asyncio
 from discord.ext.commands import Bot
 
+client = discord.Client()
 bot = Bot(command_prefix="!")
 
 
-@bot.event
+@client.event
 async def on_read():
     print("Client logged in")
 
 
-@bot.command()
-async def hello():
-    return await bot.say("Hello, world! ")
+@client.event
+async def on_message(message):
+    if message.content.startswith("Hello, Handles."):
+        await client.send_message(message.channel, "Hi, I'm Handles.")
 
 
 @bot.command()
 async def echo(*, message: str):
     await bot.say(message)
+
+
+@bot.command()
+async def hans():
+    await bot.say("Get zu flammenwerfer!")
+
+
+@bot.command()
+async def lunch():
+    await bot.say("Ask your Gideon, you bastard!")
+
+
+@bot.command()
+async def supl():
+    await bot.say("Ask your Gideon, you bastard!")
 
 
 bot.run("MzA2NDg3MDQ3MjE5MzE0NzAx.C-EeEQ.M7viDKD79MZYVC2MWTnqwMIChdI")
