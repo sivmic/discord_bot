f7aa48332b9faec4b4cab6f19037afc30d1ffe82
sivmic <sivak.dacice@outlook.com>
2017-04-26 20:24:04 +0200

    Helpp added

---
+++ controllers/helpp.py
@@ -0,0 +1,14 @@
+import controllers.controller as controller
+import os
+
+
+class Helpp(controller.Controller):
+    async def process(self, params, message, client):
+        data = next(os.walk("/home/ubuntu/handles_bot/controllers/"))[2]
+        data.remove("controller.py")
+        string = "Available commands:\n\n"
+
+        for i in range(len(data)):
+            string += "`!" + data[i].replace('.py', ' ') + "`\n"
+
+        await client.send_message(message.channel, string)
