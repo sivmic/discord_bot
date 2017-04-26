5574307eba2dcecde812dfc701a3527f14957ee2
sivmic <sivak.dacice@outlook.com>
2017-04-26 12:02:14 +0200

    Members added

--- controllers/echo.py
+++ controllers/echo.py
@@ -3,4 +3,6 @@ import controllers.controller as controller
 
 class Echo(controller.Controller):
     async def process(self, params, message, client):
-        await client.send_message(message.channel, ' '.join(params))
+        data = ' '.join(params)
+        #data += "\n\nRepeated from: " + message.author.nick
+        await client.send_message(message.channel, data)
---
+++ controllers/members.py
@@ -0,0 +1,12 @@
+import controllers.controller as controller
+
+
+class Members(controller.Controller):
+    async def process(self, params, message, client):
+        data = ""
+        server = client.get_server(message.server.id)
+
+        for member in server.members:
+            data += str(member) + "\n"
+
+        await client.send_message(message.channel, data)
