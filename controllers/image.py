29f08390ca38e8e9b8afcc07769cec878fe7c718
sivmic <sivak.dacice@outlook.com>
2017-04-27 19:23:17 +0200

    Yeah image added

--- controllers/image.py
+++ controllers/image.py
@@ -3,4 +3,7 @@ import controllers.controller as controller
 
 class Image(controller.Controller):
     async def process(self, params, message, client):
-        await client.send_file(message.channel, '/home/ubuntu/handles_bot/handles.png')
+        image_path = '/home/ubuntu/handles_bot/images/handles.png'
+        if len(params) > 1:
+            image_path = '/home/ubuntu/handles_bot/images/' + params[0]
+        await client.send_file(message.channel, image_path)
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm output.zip
 zip -r output.zip * -x "output.zip"
 scp -i "/Users/air/Google Drive/Other/aws_key.pem" output.zip ubuntu@52.59.90.213:~/handles_bot.zip
 ssh -i "/Users/air/Google Drive/Other/aws_key.pem" ubuntu@52.59.90.213 "sudo rm -rf ~/handles_bot; mkdir ~/handles_bot; unzip handles_bot.zip -d ~/handles_bot; sudo systemctl restart handles_bot.service;"
