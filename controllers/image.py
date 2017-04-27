366876f781e2022f2a70d336994a1be39f40ac1c
sivmic <sivak.dacice@outlook.com>
2017-04-27 19:24:51 +0200

    Yeah image added FINALLY

--- controllers/image.py
+++ controllers/image.py
@@ -4,6 +4,6 @@ import controllers.controller as controller
 class Image(controller.Controller):
     async def process(self, params, message, client):
         image_path = '/home/ubuntu/handles_bot/images/handles.png'
-        if len(params) > 1:
-            image_path = '/home/ubuntu/handles_bot/images/' + params[0]
+        if len(params) > 0:
+            image_path = '/home/ubuntu/handles_bot/images/' + params[0] + '.png'
         await client.send_file(message.channel, image_path)
