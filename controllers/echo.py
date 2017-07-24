55d898e60d5757ebfd7ed8ad537ca6d3e63c8da8
sivmic <sivak.dacice@outlook.com>
2017-07-24 14:59:27 +0200

    Added TTS to echo

--- controllers/echo.py
+++ controllers/echo.py
@@ -3,10 +3,15 @@ import controllers.controller as controller
 
 class Echo(controller.Controller):
     async def process(self, params, message, client):
+        tts = False
+        if params[0][0] == ";":
+            tts = True
+            params[0] = params[0][1:]
+
         data = ' '.join(params)
 
         if params[0][0] == "!":
             data = "Just.. Dude stop"
 
         await client.delete_message(message)
-        await client.send_message(message.channel, data)
+        await client.send_message(message.channel, data, tts=tts)
