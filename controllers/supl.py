4b158c4ce208de3f22af49fedf111270676fb9b4
Michal Sivák <sivak.main@gmail.com>
2017-10-05 15:38:07 +0200

    Delete supl.py

--- controllers/supl.py
+++
@@ -1,7 +0,0 @@
-import controllers.controller as controller
-
-
-class Supl(controller.Controller):
-    async def process(self, params, message, client):
-        await client.delete_message(message)
-        await client.send_message(message.channel, "Gideon, show me the future.")
