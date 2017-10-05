7334debac2864886b02eea8e098d44a277c44240
Michal Sivák <sivak.main@gmail.com>
2017-10-05 15:37:54 +0200

    Delete lunch.py

--- controllers/lunch.py
+++
@@ -1,7 +0,0 @@
-import controllers.controller as controller
-
-
-class Lunch(controller.Controller):
-    async def process(self, params, message, client):
-        await client.delete_message(message)
-        await client.send_message(message.channel, "Gideon, what's for lunch?")
